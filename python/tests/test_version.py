18d7eedbe5da97cdc0e7d415d978ab53bef53447
Daniel Hsu <dannyighsu@gmail.com>
2026-02-18 15:38:08 -0800

    fix: Remove unneeded python version test (#47)

--- python/tests/test_version.py
+++ python/tests/test_version.py
@@ -3,11 +3,6 @@
 import agent_backend
 
 
-def test_version() -> None:
-    """Test that version is defined."""
-    assert agent_backend.__version__ == "0.8.9"
-
-
 def test_package_imports() -> None:
     """Test that package can be imported."""
     assert agent_backend is not None
